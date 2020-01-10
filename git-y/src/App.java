80efb85638225374d2ec45199202b36876057df5
goott6 <goott6@goott6-PC>
2020-01-10 15:14:07 +0900

    주빈이

--- git-y/src/App.java
+++ git-y/src/App.java
@@ -3,8 +3,14 @@ public class App {
 	 public static void main(String[] args) {
 
 		System.out.println("시작");
-		System.out.println("아아~~");
 
+		System.out.println("fsdfsdfsdfdsfsdfwoeirw주빈이!!!!!!1");
+
+		System.out.println("fsdfsdfsdfdsfsdfwoeirw주빈이!!!!!!1");
+
+		System.out.println("fsdfsdfsdfdsfsdfwoeirw주빈이!!!!!!1");
+
+		System.out.println("fsdfsdfsdfdsfsdfwoeirw주빈이!!!!!!1");
 
 	}
 }
