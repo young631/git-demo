f4fa4ec8d1964c242a9914075ea7470ff429fb43
goott6 <goott6@goott6-PC>
2020-01-10 15:00:41 +0900

    주빈 수정 1

--- git-y/src/App.java
+++ git-y/src/App.java
@@ -1,7 +1,10 @@
 
 public class App {
 	 public static void main(String[] args) {
-		System.out.println("시작");
-
+	
+		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
+		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
+		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
+		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
 	}
 }
