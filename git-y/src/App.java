e6c89e73235fd61ecdcd752de6e55481ed41db32
goott6 <goott6@goott6-PC>
2020-01-10 15:14:16 +0900

    영수저어어어엉

--- git-y/src/App.java
+++ git-y/src/App.java
@@ -3,7 +3,10 @@ public class App {
 	 public static void main(String[] args) {
 
 		System.out.println("시작");
-		System.out.println("아아~~");
+		
+		
+		System.out.println("아아ㅏ영수정~~~!@~!@^%&*#@$%^&");
+		System.out.println("이히히히");
 
 
 	}
