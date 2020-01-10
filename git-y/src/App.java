03e95c09073cc360130cdcf5a40426b5edec181b
goott6 <goott6@goott6-PC>
2020-01-10 15:09:05 +0900

    ㅇㅇ

--- git-y/src/App.java
+++ git-y/src/App.java
@@ -1,10 +1,6 @@
 
 public class App {
 	 public static void main(String[] args) {
-	
-		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
-		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
-		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
-		 System.out.println("주빈이가씀!!!!!!!!!!!!!!!");
+		 
 	}
 }
