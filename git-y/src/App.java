354caa2f7e68a972abe4f7239b055f91bd3dcd0c
goott6 <goott6@goott6-PC>
2020-01-10 14:53:04 +0900

    시작

--- git-y/src/App.java
+++ git-y/src/App.java
@@ -1,8 +1,7 @@
 
 public class App {
 	 public static void main(String[] args) {
-		System.out.println("ㅎㅇㅎㅇ 영 수정123123123");
-		System.out.println("주빈이 수정");
-		System.out.println("영 수정");
+		System.out.println("시작");
+
 	}
 }
